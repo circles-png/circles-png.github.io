3f0f64469b9b9cea97d8ab97592b787c7a6e7529
Matthew Li <matthew.li10@education.nsw.gov.au>
2023-06-19 08:21:40 +1000

    update app and animations

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 const App = () => <>
-  <div className='grid place-content-center h-full bg-black text-white text-center'>
+  <div className='grid place-content-center h-full bg-black text-white text-center transition'>
     {[...Array(3).keys()].map(size => size + 1).map(
       size => [...Array(30).keys()].map(
         number => {
@@ -35,41 +35,51 @@ const App = () => <>
         }
       )
     )}
-    <div className='p-8 sm:p-16 border-t border-t-gray-300 rounded-2xl flex flex-col gap-4 sm:gap-8 backdrop-blur-sm'>
-      <h1 className='text-4xl font-bold text-center'>hi world 👋</h1>
-      <main>
-        <p>i'm <a href='https://github.com/circles-png' className='underline decoration-2 underline-offset-2' target='_blank'>circles.png</a>!</p>
-        <hr className='m-2 border-gray-500'/>
-        <div className='grid sm:grid-cols-2 gap-4 sm:gap-8 divide-gray-500'>
-          <div>
-            <span className='font-bold'>likes:</span>
-            <ul className='list-disc list-inside text-left'>
+    <div className='p-8 sm:p-16 border-t border-t-gray-300 rounded-2xl flex flex-col gap-4 sm:gap-8 backdrop-blur-sm animate-fadeIn'>
+      <h1 className='text-4xl font-bold text-center animate-fadeIn'>hi world 👋</h1>
+      <main className='animate-fadeIn'>
+        <p className='animate-fadeIn'>
+          i'm
+          <a
+            href='https://github.com/circles-png'
+            className='underline decoration-2 underline-offset-2 animate-fadeIn ml-1'
+            target='_blank'
+          >
+            circles.png
+          </a>
+          !
+        </p>
+        <hr className='m-2 border-gray-500 animate-fadeIn'/>
+        <div className='grid sm:grid-cols-2 gap-4 sm:gap-8 divide-gray-500 animate-fadeIn'>
+          <div className='animate-fadeIn'>
+            <span className='font-bold animate-fadeIn'>likes:</span>
+            <ul className='list-disc list-inside text-left animate-fadeIn'>
               <li>making things</li>
               <li>music</li>
               <li>monospace fonts</li>
               <li>gaming</li>
             </ul>
           </div>
-          <div>
-            <span className='font-bold'>dislikes:</span>
-            <ul className='list-disc list-inside text-left'>
+          <div className='animate-fadeIn'>
+            <span className='font-bold animate-fadeIn'>dislikes:</span>
+            <ul className='list-disc list-inside text-left animate-fadeIn'>
               <li>not much</li>
             </ul>
           </div>
         </div>
-        <hr className='m-2 border-gray-500' />
-        <h2 className='font-bold'>talk!</h2>
-        <div className='flex justify-center'>
+        <hr className='m-2 border-gray-500 animate-fadeIn' />
+        <h2 className='font-bold animate-fadeIn'>talk!</h2>
+        <div className='flex justify-center animate-fadeIn'>
           <a
             href='https://discordapp.com/users/262120465525506049'
-            className='decoration-[#5865F2] underline decoration-2 underline-offset-2'
+            className='decoration-[#5865F2] underline decoration-2 underline-offset-2 animate-fadeIn'
             target='_blank'
           >
             discord
           </a>
         </div>
       </main>
-      <span className='text-sm text-gray-500 text-right'>carrd? what's that?</span>
+      <span className='text-sm text-gray-500 text-right animate-fadeIn'>carrd? what's that?</span>
     </div>
   </div>
 </>
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -8,8 +8,21 @@ export default {
   safelist: ['animate-rise'],
   theme   : {
     extend: {
-      animation: { rise: 'rise 5s ease-in-out infinite' },
+      animation: {
+        fadeIn: 'fadeIn 3s cubic-bezier(0.255, 0.980, 0.160, 0.990)',
+        rise  : 'rise 5s infinite'
+      },
       keyframes: {
+        fadeIn: {
+          '0%': {
+            opacity  : 0,
+            transform: 'translateY(-20vh)'
+          },
+          '100%': {
+            opacity  : 1,
+            transform: 'translateY(0)'
+          }
+        },
         rise: {
           '0%'  : { transform: 'translateY(0)' },
           '100%': { transform: 'translateY(-100vh)' }
